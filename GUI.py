3c3a5515fcde24f1e0dc378a898ee74f22d189a7
Abraham-Saindou <abraham.saindou@laplateforme.io>
2023-03-22 20:21:08 +0100

    can see item

--- GUI.py
+++ GUI.py
@@ -1,18 +1,56 @@
 from tkinter import *
-import gestion
+from tkinter import ttk
+from gestion import Database
 
 
-class GUI(gestion.Database):
+class GUI:
     def __init__(self):
+        self.base = Database("localhost", "abraham", "abraham", "boutique")
         self.root = Tk()
         self.root.title = "Gestion des stocks"
-        self.root.geometry("650x550")
+        self.root.geometry("1200x550")
         self.root.config(bg="snow3")
 
+        self.__col_name = ['id', 'nom', 'description', 'prix en €', 'quantité', 'catégorie']
+
+        self.add_product = Button(self.root, text="Ajouter un produit")
+        self.del_product = Button(self.root, text="Supprimer un produit")
+        self.mod_product = Button(self.root, text="Modifier un produit")
+
+    def liste(self):
+
+        data = self.base.load("produit")
+        print(data)
+
+        if self.base.connector.is_connected():
+            self.box_list = ttk.Treeview(self.root, columns=self.__col_name, show='headings')
+            for index, item in enumerate(self.__col_name):
+                self.box_list.column(f"{index}", anchor=CENTER, width=200)
+                self.box_list.heading(item, text=item.upper())
+            for obj in data:
+                self.box_list.insert('', END, values=obj)
+            self.box_list.pack()
+
+    def buttons(self):
+        self.add_product.pack()
+        self.del_product.pack()
+        self.mod_product.pack()
+
+    def menu(self):
+        menu_choice = Menu(self.root)
+        self.root.config(menu=menu_choice)
+        organize_menu = Menu(menu_choice, tearoff=False)
+        menu_choice.add_cascade(label="Product Managing", menu=organize_menu)
+        organize_menu.add_command(label="Ajouter un produit")
+        organize_menu.add_command(label="Supprimer un produit")
+        organize_menu.add_command(label="Modifier un produit")
+
     def run(self):
+        self.liste()
+        self.menu()
+        self.buttons()
         self.root.mainloop()
 
 
-if __name__ == "__GUI__":
-    app = GUI()
-    app.run()
+app = GUI()
+app.run()
--- gestion.py
+++ gestion.py
@@ -1,16 +1,40 @@
 import mysql.connector
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 
 class Database:
-    def __init__(self):
-        self.connector = mysql.connector.connect(host='localhost', database='LaPlateforme', user='abraham', password='abraham')
+    def __init__(self, host, user, password, database):
+        self.connector = mysql.connector.connect(
+            host=str(host),
+            user=str(user),
+            password=str(password),
+            database=str(database),
+            )
+        self.cursor = self.connector.cursor()
 
     def connection(self):
         if self.connector.is_connected():
             cursor = self.connector.cursor()
 
-    def gui(self):
-        pass
+    def load(self, table_name):
+        self.cursor.execute("SELECT produit.id, produit.nom, produit.description, produit.prix, produit.quantite,"
+                            " categorie.nom FROM " + str(table_name) + " JOIN categorie ON id_categorie = categorie.id")
+        data = self.cursor.fetchall()
+        return data
+
+    def close_connector(self):
+        self.connector.close()
+
+    def add_product(self, nom, description, prix, quantite, id_categorie):
+        add_item = "INSERT into produit (nom, description, prix, quantite, id_categorie) VALUES(?,?,?,?,?)"
+        self.cursor.execute(add_item, (nom, description, prix, quantite, id_categorie))
+        self.connector.commit()
+
+    def del_product(self, id):
+        del_item = "DELETE FROM produit WHERE id = ?"
+        self.cursor.execute(del_item, (id,))
+        self.connector.commit()
+
+    def mod_product(self, id, nom, description, prix, quantite, categorie):
+        mod_item = "UPDATE produit SET nom = ?, description = ?, prix = ?, quantite = ?, id_categorie = ? WHERE id = ?;"
+        self.cursor.execute(mod_item, (nom, description, prix, quantite, categorie, id,))
+        self.connector.commit()
